82ae9d626e75d6e7cca410fdb997b2b42688d078
Ethan112358 <acuratnam@gmail.com>
2024-06-14 21:00:22 +0800

    Update file name

--- backend/src/main/java/com/ethan/backend/repositories/AppUserRecordsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ethan.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ethan.backend.entities.AppUser;
-
-public interface AppUserRecordsRepository extends JpaRepository<AppUser, Long> {
-}
