cdcd90e51d77b39d159db37077ad3814298b6ed4
Ethan112358 <acuratnam@gmail.com>
2024-06-14 21:24:46 +0800

    Delete unused  file

--- backend/src/main/java/com/ethan/backend/repositories/BookRecordsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ethan.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ethan.backend.entities.Book;
-
-public interface BookRecordsRepository extends JpaRepository<Book, Long> {
-}
--- backend/src/main/java/com/ethan/backend/repositories/InventoryRecordsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ethan.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ethan.backend.entities.Inventory;
-
-public interface InventoryRecordsRepository extends JpaRepository<Inventory, Long> {
-}
